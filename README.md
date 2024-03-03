b47475e135f47db5c795483fdc844ed73aad7701
nikitalokhmachev-ai <n1kkqt@gmail.com>
2024-03-03 12:17:12 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Silero VAD Implementation
+
+This repo contains two files:
+
+- `vad.py` implements real-time voice activity detection
+- `vad_pause_removal.py` inputs an audio file and outputs a file with speech without pauses
--- vad_pause_removal.py
+++ vad_pause_removal.py
@@ -5,16 +5,19 @@ from pydub import AudioSegment
 
 sampling_rate = 16000
 audiofile = 'recording.wav'
-model = torch.jit.load("silero_vad.jit")
-wav = read_audio(audiofile, sampling_rate=sampling_rate)
-song = AudioSegment.from_mp3(audiofile) 
-speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=sampling_rate, return_seconds=True)
+format = audiofile.split('.')[-1]
 
-songs = []
-for el in speech_timestamps:
-    start = int(el['start']*1000)
-    end = int(el['end']*1000)
-    songs.append(song[start:end])
+if __name__ == "__main__":
+    model = torch.jit.load("silero_vad.jit")
+    wav = read_audio(audiofile, sampling_rate=sampling_rate)
+    song = AudioSegment.from_mp3(audiofile) 
+    speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=sampling_rate, return_seconds=True)
 
-out = functools.reduce(lambda a,b: a+b, songs)
-out.export("output.wav", format="wav")
+    songs = []
+    for el in speech_timestamps:
+        start = int(el['start']*1000)
+        end = int(el['end']*1000)
+        songs.append(song[start:end])
+
+    out = functools.reduce(lambda a,b: a+b, songs)
+    out.export("output.wav", format=format)
