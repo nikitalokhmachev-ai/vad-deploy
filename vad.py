0ae5933ebca45e6058799f9e75bf4218fb5e0ddb
nikitalokhmachev-ai <n1kkqt@gmail.com>
2024-03-03 10:33:23 -0500

    vad minimalistic

---
+++ vad.py
@@ -0,0 +1,63 @@
+import numpy as np
+import torch
+torch.set_num_threads(1)
+import torchaudio
+torchaudio.set_audio_backend("soundfile")
+import pyaudio
+
+# Taken from utils_vad.py
+def validate(model,
+             inputs: torch.Tensor):
+    with torch.no_grad():
+        outs = model(inputs)
+    return outs
+
+# Provided by Alexander Veysov
+def int2float(sound):
+    abs_max = np.abs(sound).max()
+    sound = sound.astype('float32')
+    if abs_max > 0:
+        sound *= 1/32768
+    sound = sound.squeeze()  # depends on the use case
+    return sound
+
+def start_recording():
+    
+    stream = audio.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=SAMPLE_RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    data = []
+    voiced_confidences = []
+    
+    while True:
+    
+        audio_chunk = stream.read(num_samples)
+    
+        # in case you want to save the audio later
+        data.append(audio_chunk)
+    
+        audio_int16 = np.frombuffer(audio_chunk, np.int16);
+
+        audio_float32 = int2float(audio_int16)
+    
+        # get the confidences and add them to the list to plot them later
+        new_confidence = model(torch.from_numpy(audio_float32), 16000).item()
+        voiced_confidences.append(new_confidence)
+
+        print(new_confidence > 0.7)
+
+if __name__ == "__main__":
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    SAMPLE_RATE = 16000
+    CHUNK = int(SAMPLE_RATE / 10)
+    num_samples = 1536
+
+    audio = pyaudio.PyAudio()
+    model = torch.jit.load("silero_vad.jit")
+
+    start_recording()
+
